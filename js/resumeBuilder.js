8881040164b7d48d09d63a00f257ada99f24334f
xXKayPeeXx <khrishawn@hotmail.co.uk>
2016-07-07 17:09:23 +0100

    thursday
    ddd

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,16 +2,23 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 $("#main").append("Khrishawn Powell<br> ");
-var awesomethoughts="I am Khrishawn and I am AWESOME!";
+var awesomethoughts="I am Khrishawn and I am AWESOME!<br>";
 console.log(awesomethoughts)
 var funthoughts=awesomethoughts.replace("AWESOME!","FUN");
 $("#main").append(funthoughts);
 console.log(funthoughts);
 var Name = "Khrishawn Powell"
 var formattedName = HTMLheaderName.replace("%data%",Name);
-var role = "Student<br>"
+var role = "Student"
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
-console.log(formattedName);
-console.log(formattedRole);
+$("#header").prepend(formattedName)
+var HTMLskills = ["Leadership", "Critical Thinker"]
+$("#main").append(HTMLskills)
+console.log(HTMLskills);
+var bio = {"name":"Khrishawn Powell",
+ "age": 18,
+"Mobile":"02085677856",
+"Twitter": ""@Its_KP"
+"Location":"London"};
+console.log(bio);
