c9aaa411997689283e2f3eaf5a1fd48af89c1587
xXKayPeeXx <khrishawn@hotmail.co.uk>
2016-07-07 13:57:29 +0100

    next
    jj

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,9 +1,17 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-$("#main").append("Khrishawn <em>Powell</em> ");
+$("#main").append("Khrishawn Powell<br> ");
 var awesomethoughts="I am Khrishawn and I am AWESOME!";
 console.log(awesomethoughts)
 var funthoughts=awesomethoughts.replace("AWESOME!","FUN");
 $("#main").append(funthoughts);
 console.log(funthoughts);
+var Name = "Khrishawn Powell"
+var formattedName = HTMLheaderName.replace("%data%",Name);
+var role = "Student<br>"
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+console.log(formattedName);
+console.log(formattedRole);
